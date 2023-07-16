ac1570f88c261e72ed2fc039ef5d512b4cc71270
mikeFrees <mike_frees@hotmail.com>
2023-07-16 11:43:34 +0200

    js age calculation creation

--- ageConverter.js
+++ ageConverter.js
@@ -1,13 +1,34 @@
-function ConvertAge(){
+function calculateAge() {
+    const birthDate = validateInput();
+    const currentDate = new Date();
+    const ageInMilliseconds = currentDate - birthDate;
+    const age = calculateAgeComponents(ageInMilliseconds);
+    displayAge(age);
+  }
+
+  function validateInput() {
     const day = document.getElementById("day").valueAsNumber;
     const month = document.getElementById("month").valueAsNumber - 1;
     const year = document.getElementById("year").valueAsNumber;
-	const birthDate = new Date(year, month, day);
-    const currentDate = new Date();
-    const age = {years:0, months:0, days:0};
-    let difference = new Date().setMilliseconds(currentDate.getTime() - birthDate.getTime());
-    age.years = difference.getFullYear();
-    age.months = difference.getMonth();
-    age.days = difference.getDate();
-    console.log(age);
-}
+
+    return new Date(year, month, day);
+  }
+  
+  function calculateAgeComponents(milliseconds) {
+    const msInYear = 31556952000;
+    const msInMonth = 2629746000;
+    const msInDay = 86400000;
+  
+    const years = Math.floor(milliseconds / msInYear);
+    const months = Math.floor((milliseconds % msInYear) / msInMonth);
+    const days = Math.ceil(((milliseconds % msInYear) % msInMonth) / msInDay);
+  
+    return { years, months, days };
+  }
+  
+  function displayAge(age) {
+    document.getElementById("years").innerHTML = age.years.toString();
+    document.getElementById("months").innerHTML = age.months.toString();
+    document.getElementById("days").innerHTML = age.days.toString();
+  }
+  
